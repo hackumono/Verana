47756d88be0d9c16d209de2bad7b86ec1474f4a3
H3x404 <50049926+H3x404@users.noreply.github.com>
2019-10-20 11:27:24 -0300

    ~ 7x effects.

--- data/spells/scripts/attack/hells core.lua
+++ data/spells/scripts/attack/hells core.lua
@@ -1,6 +1,6 @@
 local combat = createCombatObject()
 setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_FIREDAMAGE)
-setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_FIREAREA)
+setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_EXPLOSIONAREA)
 setAttackFormula(combat, COMBAT_FORMULA_LEVELMAGIC, 5, 5, 9, 12)
 
 local area = createCombatArea(AREA_CROSS5X5)
